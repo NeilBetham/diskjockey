26f477b4ff1b64eb4529e919ab48ecf299e66089
Neil Betham <nbetham@me.com>
2014-07-08 22:11:37 -0700

    Update to rails 4.1.1

--- README.md
+++
@@ -1,2 +0,0 @@
-diskjockey [![Build Status](https://travis-ci.org/WMTU/diskjockey.svg?branch=master)](https://travis-ci.org/WMTU/diskjockey)
-==========
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Diskjockey::Application.config.session_store :cookie_store, key: '_diskjockey_session'
+Rails.application.config.session_store :cookie_store, key: '_diskjockey_session'
