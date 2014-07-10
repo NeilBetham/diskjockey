7fbd3fa18776a3148773a12d57b67b9b4d1c990a
Neil Betham <nbetham@me.com>
2014-07-09 18:11:27 -0700

    Rubocop all over my case

--- app/controllers/o_auth_controller.rb
+++ app/controllers/o_auth_controller.rb
@@ -1,9 +1,9 @@
 class OAuthController < ApplicationController
   def authorize
-    #TODO Redirect user to google o auth url
+    # TODO: Redirect user to google o auth url
   end
 
   def callback
-    #TODO Store recevied token and authorize user to use app
+    # TODO: Store recevied token and authorize user to use app
   end
 end
