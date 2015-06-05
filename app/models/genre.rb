24079a547089141bfc4300be0a8a3c9caec5b8db
Joel Yauk <jbyauk@gmail.com>
2015-06-05 10:10:37 -0400

    Fix dj app form issue

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,11 +1,12 @@
 class Genre < ActiveRecord::Base
-    GENRES = %i(
-      Alternative Ambient Blues Country
-      Eclectic Electronic Folk Hip-Hop
-      Hits Indie Jazz Local Metal Punk
-      Reggae Rock Ska Talk Urban World
-      Other
-    )
+  GENRES = %i(
+    Alternative Ambient Blues Classical
+    Comedy Country Easy\ Listening Eclectic
+    Electronic Folk Funk/Soul Hip-Hop
+    Hits Indie Jazz Local Metal Pop Punk
+    R&B Reggae Rock Ska Soundtrack Talk
+    Urban World Other
+  )
 
-    has_and_belongs_to_many :dj_applications
+  has_and_belongs_to_many :dj_applications
 end
