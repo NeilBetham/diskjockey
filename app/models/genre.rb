dd85172a737330120fcdeda517c8ef31151f3f55
Neil Betham <nbetham@me.com>
2015-06-06 22:21:47 -0700

    Add genres to seed file for easy populating

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,12 +1,3 @@
 class Genre < ActiveRecord::Base
-  GENRES = %i(
-    Alternative Ambient Blues Classical
-    Comedy Country Easy\ Listening Eclectic
-    Electronic Folk Funk/Soul Hip-Hop
-    Hits Indie Jazz Local Metal Pop Punk
-    R&B Reggae Rock Ska Soundtrack Talk
-    Urban World Other
-  )
-
   has_and_belongs_to_many :dj_applications
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+GENRES = %i(
+  Alternative Ambient Blues Classical
+  Comedy Country Easy\ Listening Eclectic
+  Electronic Folk Funk/Soul Hip-Hop
+  Hits Indie Jazz Local Metal Pop Punk
+  R&B Reggae Rock Ska Soundtrack Talk
+  Urban World Other
+)
+
+GENRES.each do |genre|
+  Genre.create name: genre
+end
