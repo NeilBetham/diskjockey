e28b88a7a4bc676c515db65a4515afc6c754304d
Neil Betham <nbetham@me.com>
2015-06-14 18:17:13 -0700

    No more bootstrap css, style updates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   skip_before_action :authenticate, only: [:new, :create]
+  layout 'login'
 
   def new
   end
--- test/controllers/api/token_controller_test.rb
+++ test/controllers/api/token_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class Api::TokenControllerTest < ActionController::TestCase
+  def setup
+    @controller = Api::TokenController.new
+  end
+
   test "should get get" do
     post :get, email: 'MyString2@mtu.edu', password: 'password'
     assert_response :success
